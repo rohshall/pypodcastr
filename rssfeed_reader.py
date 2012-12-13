235855f20d33e17a969e87586c5db5cd5d91b5b9
Salil Wadnerkar <rohshall@gmail.com>
2012-12-13 11:53:33 +0800

    added multiprocessing capability in downloading feeds

--- rssfeed_reader.py
+++ rssfeed_reader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from collections import namedtuple
 from urllib.request import urlretrieve
+import multiprocessing
 import feedparser
 
 Episode = namedtuple('Episode', ['title', 'link'])
@@ -18,10 +19,19 @@ def download_episodes(episodes):
         urlretrieve(episode.link, localfilename)
 
 
+def download_feed(feedUrl):
+    episodes = get_feed_episodes(feedUrl)
+    download_episodes(episodes)
+
+
 def get_feeds():
+    jobs = []
     for feedUrl in open('feeds.txt'):
-        episodes = get_feed_episodes(feedUrl)
-        download_episodes(episodes)
+        job = multiprocessing.Process(name=feedUrl, target=download_feed, args=(feedUrl,))
+        jobs.append(job)
+        job.start()
+    for job in jobs:
+        job.join()
 
 
 if __name__ == '__main__':
